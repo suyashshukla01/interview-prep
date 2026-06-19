3a02229ef1024b7acdc9c6c57bf66758d094ca48
Suyash Shukla <suyashshukla9245@gmail.com>
2026-06-20 01:56:45 +0530

    Ready for Render deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,15 @@
-require("dotenv").config()
+require("dotenv").config();
 
-const dns = require('dns');
-dns.setServers(['8.8.8.8','8.8.4.4']);
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
-const app = require("./src/app")
-const connectToDB = require("./src/config/database")
+const app = require("./src/app");
+const connectToDB = require("./src/config/database");
 
-connectToDB()
+connectToDB();
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
